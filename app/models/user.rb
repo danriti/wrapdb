4dee43b13526143b53ac860c7658be8c67fe08a9
Dan Riti <dmriti@gmail.com>
2012-11-17 14:03:18 -0500

    Generate rails project and add Github integration.

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  attr_accessible :image, :name, :provider, :uid
+
+  def self.from_omniauth(auth)
+    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+      user.image = auth["info"]["image"]
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyApi::Application.config.secret_token = '2df2210337217f99a0057689874f9df1e5e7c1a7c0c8ad16e3308ec40a19f475864c57266dbbd7cf036e4a0c3006d9ad804b19b108443b1a5554765704ab4663'
