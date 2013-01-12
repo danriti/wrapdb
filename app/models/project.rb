265057fb55dfcc970a0daba1348400682786c85d
Dan Riti <dmriti@gmail.com>
2013-01-12 15:50:25 -0500

    Refactor project model to use Mongoid Document.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,33 @@
-class Project < ActiveRecord::Base
-  belongs_to :user
-  has_many :endpoints
-  attr_accessible :name
+class Project
+  include Mongoid::Document
+  field :name, type: String
+  #belongs_to :user
+  #has_many :endpoints
+  has_many :instances
+
+  store_in collection: "easyAPI.users.projects"
+
+  #-----------------------------------------------------------------------------
+  # Class methods
+  #-----------------------------------------------------------------------------
+
+  # TBD
+  def self.generate
+    nil
+  end 
+
+  # TBD
+  def self.destoy_by_name
+    nil
+  end 
+
+  # TBD
+  def self.get_by_name
+    nil
+  end 
+
+  # TBD
+  def self.get_all_endpoints
+    nil
+  end 
 end
